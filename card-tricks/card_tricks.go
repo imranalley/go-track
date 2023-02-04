53530a010ad35a6ae1096d2185db47e8ea74396f
Imran Ali <imranrali1@gmail.com>
2023-02-03 23:32:55 -0500

    adding exercise 8 - slices

---
+++ card-tricks/card_tricks.go
@@ -0,0 +1,38 @@
+package cards
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int {
+	// panic("Please implement the FavoriteCards function")
+	favCards := []int{2,6,9}
+	return (favCards[:])
+}
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(slice []int, index int) int {
+	// panic("Please implement the GetItem function")
+	if index > cap(slice){
+		return -1
+	}
+	item := slice[index]
+	return(item)
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	// panic("Please implement the SetItem function")
+	slice [index] = value
+	return slice [:]
+}
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(slice []int, values ...int) []int {
+	panic("Please implement the PrependItems function")
+
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	panic("Please implement the RemoveItem function")
+}

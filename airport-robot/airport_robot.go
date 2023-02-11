fe396b7ecbae10dfe6a5430059aec8de66e20b07
Imran Ali <imranrali1@gmail.com>
2023-02-11 15:40:51 -0500

    edit

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,30 @@
 package airportrobot
 
+import (
+	"fmt"
+)
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+type Italian struct {
+	languageName string
+	name         string
+}
+
+func (i Italian) LanguageName() {
+	return fmt.Sprintf("%s", i.languageName)
+}
+
+func (i Italian) Greet(name string) {
+	return fmt.Sprintf("Ciao %s", name)
+}
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", g.LanguageName(), g.Greet(name))
+}

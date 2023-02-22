7cf40473c758c32e17ac56b9fe3aac9e7f6138d0
Imran Ali <imranrali1@gmail.com>
2023-02-22 16:40:59 -0500

    edit

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,10 +1,21 @@
 package thefarm
 
+import "errors"
+
 // See types.go for the types defined for this exercise.
 
 // TODO: Define the SillyNephewError type here.
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+	scaleMalfunc := errors.New("ErrScaleMalfunction")
+	i, err := weightFodder.FodderAmount()
+	if err != nil {
+		return 0, errors.New("non-scale error")
+	} else if err == scaleMalfunc {
+		total := i * 2 / float64(cows)
+		return total, nil
+	}
+	total := i / float64(cows)
+	return total, nil
 }

8b48b552ae894bcaeeecb12c47a61f36fc8cb801
Imran Ali <imranrali1@gmail.com>
2023-02-22 22:58:46 -0500

    edit

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -10,11 +10,15 @@ import "errors"
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
 	scaleMalfunc := errors.New("ErrScaleMalfunction")
 	i, err := weightFodder.FodderAmount()
-	if err != nil {
-		return 0, errors.New("non-scale error")
-	} else if err == scaleMalfunc {
+	if cows == 0 {
+		return 0, errors.New("division by zero")
+	} else if err == scaleMalfunc && i > 0 {
 		total := i * 2 / float64(cows)
 		return total, nil
+	} else if err != nil || err != scaleMalfunc {
+		return 0, errors.New("non-scale error")
+	} else if i < 0 {
+		return 0, errors.New("negative fodder")
 	}
 	total := i / float64(cows)
 	return total, nil

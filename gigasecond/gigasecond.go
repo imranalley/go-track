a6dc5c5f829afc3d308393dae8aa51016a8e4bd8
imranalley <imranrali1@gmail.com>
2023-11-06 23:28:55 -0500

    adding solution

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -7,27 +7,10 @@ package gigasecond
 
 // import path for the time package from the standard library
 import (
-	"fmt"
 	"time"
 )
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	dateInSeconds := t.Unix()
-
-	dateInSeconds += 1000000000
-
-	ds := fmt.Sprintf("%ds", dateInSeconds)
-	d, _ := time.ParseDuration(ds)
-	timein := t.Add(time.Second * time.Duration(d))
-
-	// // days := d.Hours()
-
-	// t = t.Add(time.Second * d)
-
-	return timein
+	return t.Add(time.Second * 1000000000)
 }

062aa88d8eab7d36b1b90cbebdaee91cec216f07
imranalley <imranrali1@gmail.com>
2023-11-10 18:56:57 -0500

    debugging

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,6 +1,7 @@
 package luhn
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,24 +11,29 @@ func Valid(id string) bool {
 	doubledStr := ""
 	sum := 0
 
-	if _, err := strconv.Atoi(id); err == nil {
-		if len(id) <= 1 {
-			return false
+	fmt.Print(len(id))
+	if len(id) <= 1 {
+		return false
+	}
+	if len(id)%2 == 0 { // if length is even
+		for index, n := range id {
+			if index%2 != 0 {
+				n *= 2
+			}
+			doubledStr += string(n)
 		}
-		runes := []rune(id)
-		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-			runes[i], runes[j] = runes[j], runes[i]
-		}
-		for index, n := range runes {
+	} else { // if length is odd
+		for index, n := range id {
 			if index%2 == 0 {
 				n *= 2
 			}
 			doubledStr += string(n)
 		}
-		for _, s := range doubledStr {
-			number, _ := strconv.Atoi(string(s))
-			sum += number
-		}
+	}
+	fmt.Printf("Number: %s", id)
+	for _, s := range doubledStr {
+		number, _ := strconv.Atoi(string(s))
+		sum += number
 	}
 	return sum%10 == 0
 }

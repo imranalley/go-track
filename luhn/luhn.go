46912c819df77a9b4aff49bf792456d8f3d18f0a
imranalley <imranrali1@gmail.com>
2023-11-10 21:25:26 -0500

    iterate sol

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,39 +1,44 @@
 package luhn
 
 import (
-	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
+
+var digitCheck = regexp.MustCompile(`^[0-9]+$`)
 
 func Valid(id string) bool {
 	id = strings.Trim(id, " ")
 	doubledStr := ""
 	sum := 0
 
-	fmt.Print(len(id))
-	if len(id) <= 1 {
-		return false
-	}
-	if len(id)%2 == 0 { // if length is even
-		for index, n := range id {
-			if index%2 != 0 {
-				n *= 2
-			}
-			doubledStr += string(n)
+	// if _, err := strconv.Atoi(id); err == nil {
+	if digitCheck.MatchString(id) {
+		if len(id) <= 1 {
+			return false
 		}
-	} else { // if length is odd
-		for index, n := range id {
-			if index%2 == 0 {
-				n *= 2
+		print(id)
+		if len(id)%2 == 0 { // if length is even
+			for index, n := range id {
+				if index%2 != 0 {
+					n *= 2
+				}
+				doubledStr += string(n)
 			}
-			doubledStr += string(n)
+		} else { // if length is odd
+			for index, n := range id {
+				if index%2 == 0 {
+					n *= 2
+				}
+				doubledStr += string(n)
+			}
 		}
-	}
-	fmt.Printf("Number: %s", id)
-	for _, s := range doubledStr {
-		number, _ := strconv.Atoi(string(s))
-		sum += number
+		print(doubledStr)
+		for _, s := range doubledStr {
+			number, _ := strconv.Atoi(string(s))
+			sum += number
+		}
 	}
 	return sum%10 == 0
 }

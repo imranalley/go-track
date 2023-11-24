bde59f5174cbbe8f8557203f5c5ae020f9b4458e
Imran Ali <zannas@Imrans-MacBook-Pro.local>
2023-11-23 22:44:19 -0500

    solve problem

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -2,23 +2,22 @@ package resistorcolor
 
 // Colors should return the list of all colors.
 func Colors() []string {
-	// colors := map[string]int {
-	// 	"black": 0,
-	// 	"brown": 1,
-	// 	"red": 2,
-	// 	"orange": 3,
-	// 	"Yellow": 4,
-	// 	"Green": 5,
-	// 	"blue": 6,
-	// 	"violet": 7,
-	// 	"grey": 8,
-	// 	"white": 9,
-	// }
-	color := []string {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
-	return color
+	return []string {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	resistor := map[string]int {
+		"black": 0,
+		"brown": 1,
+		"red": 2,
+		"orange": 3,
+		"yellow": 4,
+		"green": 5,
+		"blue": 6,
+		"violet": 7,
+		"grey": 8,
+		"white": 9,
+	}
+	return resistor[color]
 }

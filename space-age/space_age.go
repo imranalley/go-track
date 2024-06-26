b17b4c9823b8649d4b4b598072ba4f6a25e4a52e
imranalley <imranrali1@gmail.com>
2024-06-25 23:45:54 -0400

    edit

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -2,6 +2,15 @@ package space
 
 type Planet string
 
+func Orbit(planet Planet) float64{
+  if planet == "Mercury"{
+    return 0.2408467
+  }
+  if planet == "Mercury"{
+
+  }
+}
 func Age(seconds float64, planet Planet) float64 {
 	panic("Please implement the Age function")
+
 }

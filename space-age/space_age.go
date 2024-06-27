784a401f08655b0ef95ef0ed21e3be9ba24936b8
imranalley <imranrali1@gmail.com>
2024-06-26 23:52:53 -0400

    edit

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -6,10 +6,12 @@ func Orbit(planet Planet) float64{
   if planet == "Mercury"{
     return 0.2408467
   }
-  if planet == "Mercury"{
+  if planet == "Venus"{
 
   }
+
 }
+
 func Age(seconds float64, planet Planet) float64 {
 	panic("Please implement the Age function")
 

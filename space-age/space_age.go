6fd100202306bbac1f2932b6d6a287369ae913d4
imranalley <imranrali1@gmail.com>
2024-06-26 23:58:40 -0400

    edit

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -10,15 +10,24 @@ func Orbit(planet Planet) float64{
     return 0.61519726
   }
   if planet == "Earth"{
-    return 0.61519726
+    return 1
   }
   if planet == "Mars"{
-    return 0.61519726
+    return 1.8808158
   }
   if planet == "Jupiter"{
-    return 0.61519726
+    return 11.862615
   }
-
+  if planet == "Saturn"{
+    return 29.447498
+  }
+  if planet == "Uranus"{
+    return 84.016846
+  }
+  if planet == "Neptune"{
+    return 164.79132
+  }
+  return 0
 }
 
 func Age(seconds float64, planet Planet) float64 {

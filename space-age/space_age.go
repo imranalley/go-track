07b0661ed9918c53f011c3030145a6a612dddd59
imranalley <imranrali1@gmail.com>
2024-06-27 23:37:57 -0400

    edit

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -4,33 +4,33 @@ type Planet string
 
 func Orbit(planet Planet) float64{
   if planet == "Mercury"{
-    return 0.2408467*31557600
+    return 0.2408467
   }
   if planet == "Venus"{
-    return 0.61519726*31557600
+    return 0.61519726
   }
   if planet == "Earth"{
-    return 1*31557600
+    return 1
   }
   if planet == "Mars"{
-    return 1.8808158*31557600
+    return 1.8808158
   }
   if planet == "Jupiter"{
-    return 11.862615*31557600
+    return 11.862615
   }
   if planet == "Saturn"{
-    return 29.447498*31557600
+    return 29.447498
   }
   if planet == "Uranus"{
-    return 84.016846*31557600
+    return 84.016846
   }
   if planet == "Neptune"{
-    return 164.79132*31557600
+    return 164.79132
   }
   return 0
 }
 
 func Age(seconds float64, planet Planet) float64 {
   orbit_multiplier := Orbit(planet)
-  return orbit_multiplier/seconds
+  return seconds/(31557600*orbit_multiplier)
 }

55f49fb636b933ca8122ae70e10754907c159cf4
imranalley <imranrali1@gmail.com>
2024-06-29 12:38:59 -0400

    edit

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -2,10 +2,10 @@ package dna
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram
+type Histogram rune
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.

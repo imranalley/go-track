aedfed110c5e10f991fe9021d36852592fd59785
imranalley <imranrali1@gmail.com>
2024-07-04 00:02:45 -0400

    edit

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,21 +1,18 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-  abs_diff := x-y
-  if abs_diff<0 {
-    abs_diff=-abs_diff
-  }
-  if x<1 && y<1{
+  abs_diff := math.Sqrt(x*x + y*y)
+  
+  if abs_diff<1 {
     return 10
   }
-  if x>10 || y>10{
-    return 0
+  if abs_diff <=5 {
+    return 5
   }
-  if (x>5 && x<=10) || (y>5 && y<=10){
+  if abs_diff <=10 {
     return 1
-  }
-  if abs_diff>10{
-    return 0
   }
   return 0
 }

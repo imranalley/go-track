6705d42d27a47a451621abfe08eb65ea2a51a537
imranalley <imranrali1@gmail.com>
2024-07-03 23:57:31 -0400

    edit

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,6 +1,10 @@
 package darts
 
 func Score(x, y float64) int {
+  abs_diff := x-y
+  if abs_diff<0 {
+    abs_diff=-abs_diff
+  }
   if x<1 && y<1{
     return 10
   }
@@ -9,6 +13,9 @@ func Score(x, y float64) int {
   }
   if (x>5 && x<=10) || (y>5 && y<=10){
     return 1
+  }
+  if abs_diff>10{
+    return 0
   }
   return 0
 }
